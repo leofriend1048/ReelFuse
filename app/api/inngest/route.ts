69bc807775e91782c0875bf3c539a737d70f928e
LeofriendMTB <120510202+LeofriendMTB@users.noreply.github.com>
2024-11-22 19:08:34 +0800

    Allow streaming for inngest api route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,10 +2,13 @@ import { serve } from "inngest/next";
 import { inngest } from "@/src/inngest/client";
 import { videoIteration, videoLibraryProcessing } from "@/src/inngest/functions";
 
+export const runtime = "edge";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     videoIteration,
     videoLibraryProcessing,
   ],
+  streaming: "allow",
 });
