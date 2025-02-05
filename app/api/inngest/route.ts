b091b7ad557fe03d85db67e1aa9ea1f64af31c5d
LeofriendMTB <120510202+LeofriendMTB@users.noreply.github.com>
2025-02-06 06:54:12 +0800

    Set ingest runtime to edge

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "@/src/inngest/client";
 import { videoLibraryProcessing } from "@/src/inngest/functions";
 
+export const runtime = 'edge' 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
